03972da94a5ff327ba106625fd49297b55510801
Can Aykut, INI-CIT-EAS <Aykut.Can@swisscom.com>
2024-05-29 19:03:37 +0200

    Add task-3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let booksOfAuthor = []
+  Object.values(books).forEach(book => {
+    if(book.author == author){
+      booksOfAuthor.push(book)
+    }
+  });
+  if (booksOfAuthor.length > 0){
+    res.status(200).send(JSON.stringify(booksOfAuthor, null, 2));
+  }else{
+    res.status(404).json({message: "Book not found belongs author"})
+  }
 });
 
 // Get all books based on title
