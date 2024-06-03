b2f6d09fd799b7b2d204b6ca47c8dc2254e70874
Can Aykut, INI-CIT-EAS <Aykut.Can@swisscom.com>
2024-06-03 22:46:59 +0200

    Task-9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -34,7 +34,7 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
   const { isbn } = req.params;
   const { review } = req.query;
-  const { username } = req.user.username;
+  const { username } = req.user;
 
   if(!isbn || !review){
     return res.status(400).json({message: "ISBN and review are required"})
@@ -48,6 +48,33 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
   book.reviews[username] = review;
   const response = { message: "Review added successfully", reviews: book.reviews[username] };
+  return res.status(200).send(JSON.stringify(response, null, 2));
+
+});
+
+// delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const { isbn } = req.params;
+  const { username } = req.user;
+
+  if(!isbn){
+    return res.status(400).json({message: "ISBN and review are required"})
+  }
+
+  if(!books[isbn]){
+    return res.status(404).json({message: "Book not found"})
+  }
+
+  const book = books[isbn];
+
+  // Check if the review by the user exists
+  if (!book.reviews[username]) {
+    return res.status(404).json({ message: "Review not found" });
+  }
+
+
+  delete book.reviews[username];
+  const response = {  message: "Review deleted successfully", reviews: book.reviews[username]};
   return res.status(200).send(JSON.stringify(response, null, 2));
 
 });
